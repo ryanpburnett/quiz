db94a67fa03c0a6e02ed3783b3d713883d48158a
Ryan <ryanpburnett@yahoo.com>
2020-12-03 23:59:25 -0600

    updated javascript

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
-var h2 = document.getElementById('timerDown')
-var countDown = 100
+// var h2 = document.getElementById('timerDown')
+// var countDown = 100
 
-setInterval(function() {
-    countDown--
-    h2.innerText = "Timer: " + countDown;
-}, 1000)
+// setInterval(function() {
+//     countDown--
+//     h2.innerText = "Timer: " + countDown;
+// }, 1000)
 
-var BigRedButton = document.getElementById('start')
+// var BigRedButton = document.getElementById('start')
 
-var questions = [
-    {
-        question1: "The answer to this question is 'A'",
-        choices: ['A', 'B', 'C'],
-    }
-]
+// var questions = [
+//     {
+//         question: "The answer to this question is 'A'",
+//         choices: ['A', 'B', 'C'],
+//         answer: 'A'
+//     }
+// ]
 
-BigRedButton.addEventListener('click', function() {         
-    document.getElementById("qBlock").innerHTML = questions[0]
+// BigRedButton.addEventListener('click', function() {         
+//     document.getElementById("qBlock").innerHTML = questions[0]
+// })
+
+var startButton = document.getElementById('start')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
+
+startButton.addEventListener('click', startQuiz)
+
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
+
+var questions = [
+  {
+    question: 'The answer to this question is A.',
+    answers: [
+      { text: 'A', correct: true },
+      { text: 'B', correct: false }
+    ]
+  },
+  {
+    question: 'is this a question?',
+    answers: [
+      { text: 'Yes', correct: true },
+      { text: 'No', correct: false }
+    ]
+  },
+]
